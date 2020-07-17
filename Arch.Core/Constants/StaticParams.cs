ef8162b5f55a50db61c2e0a5bb804ead74876496
Egehan Gungor <egehangngrr@gmail.com>
2020-07-17 15:34:16 +0300

    Initial commit

---
+++ Arch.Core/Constants/StaticParams.cs
@@ -0,0 +1,9 @@
+﻿namespace Arch.Core.Constants
+{
+    public struct StaticParams
+    {
+        public const string Dashboard = "/Dashboard";
+        public const string SifrelemeParametresi = "testfalan";
+        public const string DefaultConnstr = "Data Source=DESKTOP-D7GKO0L;initial Catalog=WorkDB;user id=sa;password=123;MultipleActiveResultSets=True";
+    }
+}
